cca204faeedefe269192631d7a839d9de140440e
sallamander <ssall@alumni.nd.edu>
2015-08-18 17:31:46 -0600

    Add function to get basefile_name from directories

--- code/read_data.py
+++ code/read_data.py
@@ -18,7 +18,23 @@ def read_file(filepath):
 
 	return df
 
+def get_basefile_name(year): 
+	'''
+	Input: Integer
+	Output: String
+
+	Take in the given year, navigate to the satellite data for that year, and 
+	pull the basefile name that the shapefile library will need to put all 
+	of the files together. 
+	'''
+	current_dir = '../../data/raw_data/MODIS/' + str(year) + '/'
+	files = os.listdir(current_dir)
+
+	ext_index = files[0].find('.')
+	basefile_name = files[0][:ext_index]
+	return basefile_name
+
 if __name__ == '__main__': 
 	for year in range(2001, 2016): 
-		cwd = '../../data/raw_data/MODIS/' + str(year) + '/'
-		print os.listdir(cwd)
+		print get_basefile_name(year)
+		
