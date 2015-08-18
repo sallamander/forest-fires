0ede1e741601e61a757cd9f3286f347ea153a8f5
sallamander <ssall@alumni.nd.edu>
2015-08-18 17:04:38 -0600

    Put create_dirs in a function

--- code/create_dirs.py
+++ code/create_dirs.py
@@ -1,10 +1,19 @@
 import os
 
-for year in range(2001, 2016): 
-	os.makedirs('../../data/raw_data/MODIS/' + str(year))
+def create_dirs(): 
+	'''
+	Input: None
+	Output: Folders 
 
-for year in range(2012, 2016): 
-	os.makedirs('../../data/raw_data/VIIRS/' + str(year))
+	Create a folder for each year of data that I will be grabbing, and for 
+	each type of satellite (MODIS, VIIRS, or VIIRS_I). 
+	'''
 
-for year in range(2014, 2016): 
-	os.makedirs('../../data/raw_data/VIIRS_I/' + str(year))
+	for year in range(2001, 2016): 
+		os.makedirs('../../data/raw_data/MODIS/' + str(year))
+
+	for year in range(2012, 2016): 
+		os.makedirs('../../data/raw_data/VIIRS/' + str(year))
+
+	for year in range(2014, 2016): 
+		os.makedirs('../../data/raw_data/VIIRS_I/' + str(year))
