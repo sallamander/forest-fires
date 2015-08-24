5f1d5d8b85a1c34da0f46dc604af4169e2450038
sallamander <ssall@alumni.nd.edu>
2015-08-24 14:48:39 -0600

    Setup state adding method for MODIS pickled dfs

--- code/translate_data.py
+++ code/translate_data.py
@@ -1,4 +1,8 @@
 import reverse_geocoder as rg
+import simplejson
+import pickle
+from requests import get
+from collections import defaultdict
 
 def get_reverse_geocode(df): 
 	'''
@@ -9,11 +13,11 @@ def get_reverse_geocode(df):
 	and county information and put those in the DataFrame. 
 	'''
 
+	print 'In Reverse Geocode'
+
 	geo_info = defaultdict(list)
-	df = df.loc[0:50].copy()
 	for idx, lat in enumerate(df['LAT']): 
 		coords = (lat, df.loc[idx, 'LONG'])
-		print idx, coords
 		results = rg.search(coords)[0]
 
 
@@ -27,3 +31,29 @@ def get_reverse_geocode(df):
 	df['county'] = geo_info['county']
 
 	return df
+
+def get_weather_data(): 
+	'''
+	Input: Pandas DataFrame
+	Output: 
+
+	Take the latitude/longitude and time data for each row in the DataFrame, and get the weather 
+	for that pairing. I want to go three days back in time for each row in the DataFrame. 
+	'''
+
+	with open('/Users/sallamander/apis/access/forecast_io.json') as f: 
+		pass 
+	return file_json
+
+
+if __name__ == '__main__': 
+	for year in xrange(2015, 2012, -1):
+		with open('../../data/pickled_data/MODIS/df_' + str(year) + '.pkl') as f: 
+			df = pickle.load(f)
+			df = get_reverse_geocode(df)
+			print 'Finished Year... ' + str(year)
+			with open('../../data/pickled_data/MODIS/df_' + str(year) + '_st.pkl', 'w+') as f2: 
+				pickle.dump(df, f2)
+
+
+
