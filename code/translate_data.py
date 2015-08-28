14a97bc941fde70f4205713c1783478917ece81d
sallamander <ssall@alumni.nd.edu>
2015-08-28 10:10:45 -0600

    Delete reverse geocode method; working through sql would be better

--- code/translate_data.py
+++ code/translate_data.py
@@ -4,34 +4,6 @@ import pickle
 from requests import get
 from collections import defaultdict
 
-def get_reverse_geocode(df): 
-	'''
-	Input: Pandas DataFrame
-	Output: Pandas DataFrame
-
-	Take the latitude/longitude coordinates for each row in the DataFrame, and get the country, state, 
-	and county information and put those in the DataFrame. 
-	'''
-
-	print 'In Reverse Geocode'
-
-	geo_info = defaultdict(list)
-	for idx, lat in enumerate(df['LAT']): 
-		coords = (lat, df.loc[idx, 'LONG'])
-		results = rg.search(coords)[0]
-
-
-		geo_info['country'].append(results['cc'])
-		geo_info['state'].append(results['admin1'])
-		geo_info['county'].append(results['admin2'])
-
-
-	df['country'] = geo_info['country']
-	df['state'] = geo_info['state']
-	df['county'] = geo_info['county']
-
-	return df
-
 def get_weather_data(): 
 	'''
 	Input: Pandas DataFrame
