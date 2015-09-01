362ce1fa4ac28003636a730736a57e5f3d049e0f
sallamander <ssall@alumni.nd.edu>
2015-09-01 05:52:39 -0600

    Split out pandas versus postgres df functionality into different files

--- code/create_pandas_dfs.py
+++ code/create_pandas_dfs.py
@@ -1,27 +1,3 @@
-import pandas as pd
-import pickle
-import psycopg2
-
-def merge_df():
-	'''
-	Input: None
-	Output: PSQL Data Table
-	'''
-	conn = psycopg2.connect('dbname=forest_fires')
-	cursor = conn.cursor()
-
-	cursor.execute('''CREATE TABLE forest_st_cnty AS
-					 (SELECT points.*, polys.fire_name
-					 FROM detected_fires_2013 as points
-							INNER JOIN daily_fire_shapefiles_2013 as polys
-					 ON points.date = polys.date_ 
-					 	AND ST_WITHIN(points.wkb_geometry, polys.wkb_geometry));
-					''')
-
-	conn.commit()
-	conn.close()
-
-
 def format_df(df): 
 	'''
 	Input: Pandas DataFrame
@@ -49,6 +25,3 @@ def pickle_df_sf(year, df):
 
 	with open('../data/pickled_data/MODIS/' + 'df_' + str(year) + '.pkl', 'w+') as f: 
 		pickle.dump(df, f)
-
-if __name__ == '__main__': 
-	merge_df()
