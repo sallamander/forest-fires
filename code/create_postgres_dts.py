fbfaa8b53e4543bd00e6325fd4920b45b2ed0cf8
sallamander <ssall@alumni.nd.edu>
2015-09-01 05:54:26 -0600

    Rename create_dbs.py and remove code that has been moved to other files

--- code/create_postgres_dts.py
+++ code/create_postgres_dts.py
@@ -66,52 +66,6 @@ def create_db(filepath, db_name, latin_encoding=False):
 
 	os.system(create_db_command)
 
-def merge_fire_perimeters(year): 
-	'''
-	Input: Integer
-	Output: PSQL Table
-
-	Merge on the fire perimeters for the given year to that years table in the forest_fire database. 
-	'''
-
-	conn = psycopg2.connect('dbname=forest_fires')
-	cursor = conn.cursor()
-
-	cursor.execute('''CREATE TABLE forest_st_cnty AS
-					 (SELECT points.*, polys.countyfp, polys.statefp
-					 FROM forest_fires as points
-							JOIN county as polys
-					 ON ST_WITHIN(points.wkb_geometry, polys.wkb_geometry));
-					''')
-
-def format_df(df): 
-	'''
-	Input: Pandas DataFrame
-	Output: Pandas DataFrame
-
-	In the years 2008 and prior, the data is formatted slightly differently than later years. The 
-	Fire_ and Fire_ID variables from later years are named differently, as are the LAT and LONG 
-	variables. For these, we simply need to change names. For the TEMP variable from years 2009+, we need to 
-	use the T21 from the years prior to 2008, and for the JULIAN varible in years 2009+, we need to parse
-	part of the JDATE variable. 
-	'''
-
-	df = df.rename(columns={'MCD14ML_': 'FIRE_', 'MCD14ML_ID': 'FIRE_ID', 'WGS84LAT': 'LAT', 'WGS84LONG': 'LONG', 
-							'T21': 'TEMP', 'UTC': 'GMT', 'SATELLITE': 'SAT_SRC', 'CONFIDENCE': 'CONF'})
-	df['JULIAN'] = df['JDATE'].apply(lambda x: int(str(x)[-3:]))
-	df = df.drop(['T31', 'JDATE'], axis=1)
-
-	return df
-
-def pickle_df_sf(year, df): 
-	'''
-	Input: Integer, Pandas DataFrame
-	Ouput: Pickled file of DataFrame
-	'''
-
-	with open('../data/pickled_data/MODIS/' + 'df_' + str(year) + '.pkl', 'w+') as f: 
-		pickle.dump(df, f)
-
 if __name__ == '__main__': 
 	year = sys.argv[1]
 	create_fire_db(year)
