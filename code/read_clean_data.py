84afc73f2ed01d19d7a477bfa7fce5e51c3ab7cb
sallamander <ssall@alumni.nd.edu>
2015-08-31 18:21:10 -0600

    Finalize functions for reading all shapefiles into dbs

--- code/read_clean_data.py
+++ code/read_clean_data.py
@@ -15,7 +15,7 @@ def create_fire_db(year, basefile_name):
 	The basefile_name read in will point to a set of shapefiles. This function will read those shapefiles
 	into a psql table. 
 	'''
-	filepath = '../../data/raw_data/MODIS/' + str(year) + '/' + basefile_name
+	filepath = '../../data/raw_data/MODIS/' + str(year)
 
 	db_name = 'detected_fires_' + str(year)
 	create_db(filepath, db_name)
@@ -29,20 +29,23 @@ def create_shapefile_db(name, year):
 	psql table from it. The county shapefile contains state labels as well, so that's why I'm not reading in 
 	any state shapefiles. 
 	'''
+	latin_encoding = False
 
 	if name == 'fire': 
 		filepath = '../../data/boundary_files/forest_fires/end_boundaries/' + str(year)
 	elif name == 'county': 
 		filepath = '../../data/boundary_files/county/' + str(year)
+		latin_encoding = True
 	elif name == 'urban': 
-		filepath = '../../data/boundary_files/urban_areas' + str(year)
+		latin_encoding = True
+		filepath = '../../data/boundary_files/urban_areas/' + str(year)
 	else: 
 		raise Exception('No boundary folder name put in... Try again!')
 
 	db_name = name + '_shapefiles_' + str(year)
-	create_db(filepath, db_name)
+	create_db(filepath, db_name, latin_encoding)
 
-def create_db(filepath, db_name): 
+def create_db(filepath, db_name, latin_encoding=False): 
 	'''
 	Input: String, String
 	Output: PSQL Table
@@ -54,8 +57,14 @@ def create_db(filepath, db_name):
 	# I'm using the overwrite option here because anytime this I'm creating a db I'll be fixing something 
 	# and need to create the database from scratch. Otherwise, I won't be running this function. 
 	create_db_command = 'ogr2ogr -f "PostgreSQL" PG:"dbname=forest_fires user=' + os.environ['USER'] + \
-						'" "/Users/' + os.environ['USER'] + filepath \
+						'"' + ' ' + filepath \
 						+ ' -nlt PROMOTE_TO_MULTI -nln ' + db_name + ' -overwrite'
+
+	# Some of the shapefiles (county and urban area) need to be latin-encoded to read in correctly. 
+	if latin_encoding:
+		create_db_command = 'PGCLIENTENCODING=LATIN1 ogr2ogr -f "PostgreSQL" PG:"dbname=forest_fires user=' + os.environ['USER'] + \
+					'"' + ' ' + filepath \
+					+ ' -nlt PROMOTE_TO_MULTI -nln ' + db_name + ' -overwrite'
 
 	os.system(create_db_command)
 
@@ -126,6 +135,9 @@ if __name__ == '__main__':
 	year = sys.argv[1]	
 	basefile_name = get_basefile_name(year)
 	create_fire_db(year, basefile_name)
+	create_shapefile_db('fire', year)
+	create_shapefile_db('county', year)
+	create_shapefile_db('urban', year)
 	# pickle_df_sf(year, shapefile_df)
 
 		
