052aedc9b89b532d2364806ef556ec91c3d4221a
sallamander <ssall@alumni.nd.edu>
2015-09-01 20:54:22 -0600

    Get eda flask app up and running

--- code/eda/duplicate_boundaries_app/duplicates_app.py
+++ code/eda/duplicate_boundaries_app/duplicates_app.py
@@ -2,19 +2,16 @@ from flask import Flask, render_template
 import pickle
 import numpy as np
 import json
+import os
 app = Flask(__name__)
 
-api_key = get_api_key()
+api_key = os.environ['MAPBOX_API_ACCESS']
 print api_key
 
 @app.route('/')
 def homepate(): 
 	return render_template('index.html', api_key=api_key)
 
-def get_api_key(): 
-	with open('/Users/sallamander/apis/access/mapbox.json') as f: 
-		mapbox_json = json.loads(f)
-		return mapbox_json['access-token']
 
 if __name__ == '__main__': 
 	app.run()
