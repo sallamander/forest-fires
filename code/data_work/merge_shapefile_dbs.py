f37226c01279790ffb6b0de64cb51ce95d919a03
sallamander <ssall@alumni.nd.edu>
2015-09-03 13:12:05 -0600

    Initial working query to merge on fire perimeters

--- code/data_work/merge_shapefile_dbs.py
+++ code/data_work/merge_shapefile_dbs.py
@@ -10,12 +10,22 @@ def merge_df():
 	conn = psycopg2.connect('dbname=forest_fires')
 	cursor = conn.cursor()
 
-	cursor.execute('''CREATE TABLE forest_st_cnty AS
+	cursor.execute(''' CREATE TABLE merged_boundary AS
+						
+					(SELECT DISTINCT lat FROM 
 					 (SELECT points.*, polys.fire_name
 					 FROM detected_fires_2013 as points
-							INNER JOIN daily_fire_shapefiles_2013 as polys
+							INNER JOIN perimeters_shapefiles_2013 as polys
 					 ON points.date = polys.date_ 
-					 	AND ST_WITHIN(points.wkb_geometry, polys.wkb_geometry));
+					 	AND ST_WITHIN(points.wkb_geometry, polys.wkb_geometry)) as temp_fires);  
+
+					CREATE TABLE ff2 as 
+						SELECT df.*, 
+							CASE WHEN ff.lat IS NOT NULL THEN TRUE
+								 WHEN ff.lat IS NULL THEN FALSE END AS fire
+						FROM detected_fires_2013 df
+						LEFT JOIN ff 
+							ON df.lat = ff.lat; 
 					''')
 
 	conn.commit()
