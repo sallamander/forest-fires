a9a2ee63bc8789bc1a199aacd283d36c2d70561e
sallamander <ssall@alumni.nd.edu>
2015-09-03 13:48:54 -0600

    Working query to merge on fire perimeters and save result as variable; just need to re-factor to make cleaner

--- code/data_work/merge_shapefile_dbs.py
+++ code/data_work/merge_shapefile_dbs.py
@@ -8,7 +8,7 @@ For trues/falses (whether fire or not and whether urban area), input
 variable name and table to query from. 
 '''
 
-def merge_df():
+def boundary_bool_merge(new_col_name, shapefile_table_name, detected_fires_table):
 	'''
 	Input: None
 	Output: PSQL Data Table
@@ -16,26 +16,29 @@ def merge_df():
 	conn = psycopg2.connect('dbname=forest_fires')
 	cursor = conn.cursor()
 
-	cursor.execute(''' CREATE TABLE merged_boundary AS
+	cursor.execute(''' CREATE TABLE ff AS
 						
-					(SELECT DISTINCT lat FROM 
+					(SELECT DISTINCT lat, long, date FROM 
 					 (SELECT points.*, polys.fire_name
-					 FROM detected_fires_2013 as points
-							INNER JOIN perimeters_shapefiles_2013 as polys
+					 FROM ''' + detected_fires_table + '''  as points
+							INNER JOIN ''' + shapefile_table_name + ''' as polys
 					 ON points.date = polys.date_ 
 					 	AND ST_WITHIN(points.wkb_geometry, polys.wkb_geometry)) as temp_fires);  
 
 					CREATE TABLE ff2 as 
 						SELECT df.*, 
 							CASE WHEN ff.lat IS NOT NULL THEN TRUE
-								 WHEN ff.lat IS NULL THEN FALSE END AS fire
+								 WHEN ff.lat IS NULL THEN FALSE END AS ''' + new_col_name + 
+					'''
 						FROM detected_fires_2013 df
 						LEFT JOIN ff 
-							ON df.lat = ff.lat; 
+							ON df.lat = ff.lat
+							AND df.long = ff.long
+							AND df.date = ff.dat; 
 					''')
 
 	conn.commit()
 	conn.close()
 
 if __name__ == '__main__': 
-	merge_df()
+	boundary_bool_merge('fire', 'perimeters_shapefiles_2013', 'detected_fires_2013')
