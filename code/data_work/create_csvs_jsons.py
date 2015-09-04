68740c4b08895a7bbec102b754b102700fbc1bf5
sallamander <ssall@alumni.nd.edu>
2015-09-04 14:24:55 -0600

    Add output_json function to create_csvs_json.py; need to build out dependent functions

--- code/data_work/create_csvs_jsons.py
+++ code/data_work/create_csvs_jsons.py
@@ -1,5 +1,7 @@
 import psycopg2
 import os
+import json 
+import pandas as pd
 
 def output_csv(year): 
 	'''
@@ -17,6 +19,28 @@ def output_csv(year):
 	conn.execute('''COPY detected_fires_{year} to 
 				'../data/csvs/detected_fires_{year}.csv' DELIMITER AS ',' 
 				CSV HEADER;'''.format(str(year)))
+
+def output_json(year, geo_type):
+	'''
+	Input: Integer, String
+	Output: JSON File
+
+	For the given year and geography type, create JSON files that hold the geoJSON for mapping 
+	that geography type. 
+	'''
+ 		
+ 	query = get_json_query(geo_type)
+
+ 	df = pd.read_sql(map_query, conn)
+
+    list_to_export = []	
+    for idx, row in df.iterrows():
+       	list_to_export.append((add_properties_geo(row)))
+
+    filename = geo_type + '.json'
+    filepath = get_filepath(filename)
+    with open(filepath, 'w+') as f: 
+    	f.write(json.dumps(list_to_export))
 
 def check_dir(data_dir_name): 
 	'''
@@ -39,7 +63,6 @@ def check_dir(data_dir_name):
 		data_dirs = os.listdir(current_dir[:location] + '/forest-fires/data/')
 		if data_dir_name not in data_dirs: 
 			os.mkdir(current_dir[:location] + '/forest-fires/data/' + data_dir_name)
-
 
 if __name__ == '__main__': 
 	for year in xrange(2013, 2015): 
