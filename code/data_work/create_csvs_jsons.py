26eaeb7d92d9a5d3a450826fa22d6000df9717f5
sallamander <ssall@alumni.nd.edu>
2015-09-04 12:09:45 -0600

    Generalize check_dir function

--- code/data_work/create_csvs_jsons.py
+++ code/data_work/create_csvs_jsons.py
@@ -9,7 +9,7 @@ def output_csv(year):
 	Ouptut the postgres detected_fires table for the inputted year into a .csv. 
 	'''
 
-	check_csv_dir()
+	check_dir('csvs')
 
 	conn = pscycopg2.connect('dbname=forest_fires', user=os.environ['USER'], host='localhost')
 	cursor = conn.cursor()
@@ -18,17 +18,17 @@ def output_csv(year):
 				'../data/csvs/detected_fires_{year}.csv' DELIMITER AS ',' 
 				CSV HEADER;'''.format(str(year)))
 
-def check_csv_dir(): 
+def check_dir(data_dir_name): 
 	'''
 	Input: None
 	Output: Possibly Created Directory/Folder
 
-	Check to make sure the .csv directory/folder is created, and if not create it. 
+	Check to make sure the data/data_dir_name directory/folder is created, and if not create it. 
 	'''
 
 	# In the hopes of generalizing this so I can easily throw it up on AWS or somebody else can 
-	# use it, I'm going to find the current directory path, remove any directories forest-fires
-	# and deeper, and then create the .csv in forest-fires/data/csv. 
+	# use it, I'm going to find the current directory path, remove any directories /forest-fires
+	# and deeper, and then create the data_dir_name in forest-fires/data/data_dir_name. 
 	current_dir = os.getcwd()
 	location = current_dir.find('forest-fires')
 
@@ -37,8 +37,9 @@ def check_csv_dir():
 						Smokey would be ashamed!")
 	else: 
 		data_dirs = os.listdir(current_dir[:location] + '/forest-fires/data/')
-		if 'csvs' not in data_dirs: 
-			os.mkdir(current_dir[:location] + '/forest-fires/data/csvs')
+		if data_dir_name not in data_dirs: 
+			os.mkdir(current_dir[:location] + '/forest-fires/data/' + data_dir_name)
+
 
 if __name__ == '__main__': 
 	for year in xrange(2013, 2015): 
