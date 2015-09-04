99043370cf5f55c24e41e99e8bc332911bdb49dc
sallamander <ssall@alumni.nd.edu>
2015-09-04 14:50:26 -0600

    Initial commit clean_merged_dts.py

---
+++ code/data_work/clean_merged_dts.py
@@ -0,0 +1,13 @@
+import pscyopg2
+
+def add_stname_county(year): 
+	'''
+	Input: Integer
+	Output: Updated DataTable
+
+	Merge on the state name to the county_shapefiles table, so that we have the state name associated
+	with each county, and not just the state fips. 
+	'''
+
+	pass
+
