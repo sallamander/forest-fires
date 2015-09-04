463b865acae08b4faf1ad64f7770cbed44bf01b9
sallamander <ssall@alumni.nd.edu>
2015-09-04 14:57:29 -0600

    Finish add_stname_county function

--- code/data_work/clean_merged_dts.py
+++ code/data_work/clean_merged_dts.py
@@ -9,5 +9,26 @@ def add_stname_county(year):
 	with each county, and not just the state fips. 
 	'''
 
-	pass
+	conn = pscyopg2.connect(dbname='forest_fires', user=os.environ['USER'], host='localhost')
+	cursor = conn.cursor()
+
+	county_table = 'county_shapefiles_' + str(year)
+	state_table = 'state_shapefiles_' + str(year)
+	cursor.execute(''' ALTER TABLE {county_table} 
+						ADD state_name varchar(30) AS 
+							(SELECT st.name 
+							FROM {county_table} ct 
+							INNER JOIN {state_table} st 
+								ON ct.statefp = st.statefp)
+					;'''.format(county_table=county_table, state_table=state_table)
+
+	conn.commit()
+	conn.close()
+
+if __name__ == '__main__': 
+	for year in xrange(2013, 2015): 
+		add_stname_county(year)
+
+		
+ 
 
