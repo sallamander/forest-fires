24a285d85d6b3591cf7553e19bbe4991fb1c015d
sallamander <ssall@alumni.nd.edu>
2015-09-05 13:40:13 -0600

    Played with color and path for states in D3 practice app

--- code/eda/practice_app/templates/index_d3_js.js
+++ code/eda/practice_app/templates/index_d3_js.js
@@ -1,5 +1,5 @@
 //Width and height
-var w = 1000;
+var w = 500;
 var h = 500;
 
 //Create SVG element
@@ -8,14 +8,20 @@ var svg = d3.select("body")
 			.attr("width", w)
 			.attr("height", h);
 
-var path = d3.geo.path(); 
+var projection = d3.geo.albersUsa()
+						.translate([w/2, h/2])
+						.scale([w]); 	
 
-
+var path = d3.geo.path().projection(projection); 
+color = d3.rgb(120, 175, 120)
 
 d3.json('/data/jsons/2013_state.json', function(json){
 	svg.selectAll("path")
 	.data(json)
 	.enter()
 	.append("path")
-	.attr("d", path); 
+	.attr("d", path)
+	.style("fill", color)
+	.style('stroke-width', '3') 
 }); 
+
