058248d0a6f524fb83c24de993d267d993fa63c9
sallamander <ssall@alumni.nd.edu>
2015-09-08 17:37:59 -0600

    Add data_manip folder

---
+++ code/data_manip/geo_featurization.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def transform_variables(df, **kwargs):
+	'''
+	Input: Pandas DataFrame, Dictionary
+	Output:  Pandas DataFrame
+
+	Transform the pandas dataframe columns using the passed in dictionary, where the keys are the column names
+	and the values are options of how to transform that data (i.e. dummy it, or something like that). 
+	'''
+	pass
