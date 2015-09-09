d540feaf6fe4a4a967bca0a9a182098469ee125e
sallamander <ssall@alumni.nd.edu>
2015-09-09 09:03:13 -0600

    Add tt_split_all_less60 function to code/modeling/data_manip/tt_splits.py

--- code/modeling/data_manip/tt_splits.py
+++ code/modeling/data_manip/tt_splits.py
@@ -1,5 +1,21 @@
-def tt_split_all_less2(df): 
-	pass
-	
+from datetime import date
+from pandas import Timedelta
+
+def tt_split_all_less60(df): 
+	'''
+	Input: Pandas Dataframe
+	Output: Pandas DataFrame, Pandas DataFrame
+
+	Split the pandas data frame into a train/test split where we train on all of the 
+	data but the most recent 60 days
+	'''
+
+	today = date.today()
+	today_less60 = today - Timedelta(days=60)
+	train = df.query('date < @today_less60')
+	test = df.query('date >= @today_less60')
+
+	return train, test
+
 def tt_split_early_late(): 
 	pass
