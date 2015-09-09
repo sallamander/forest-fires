988b9fb48aa3be13061be1826a54202840e61eaa
sallamander <ssall@alumni.nd.edu>
2015-09-09 15:41:50 -0600

    Write get_unique_pairs function

--- code/data_setup/grab_weather_data.py
+++ code/data_setup/grab_weather_data.py
@@ -2,6 +2,8 @@ from requests import get
 import psycopg2
 import pandas as pd
 import multiprocessing
+import sys
+import pickle
 
 def get_lat_long_time(year): 
 	'''
@@ -11,18 +13,30 @@ def get_lat_long_time(year):
 	Read in the Pandas DataFrame for a given year, and grab the latitude, longitude, and 
 	date to call the forecast.io with and get weather data. 
 	'''
-	df_filepath = '../../data/csvs/fires_' + str(year)
+	df_filepath = '../../data/csvs/fires_' + str(year) + '.csv'
 
 	df = pd.read_csv(df_filepath)
 
 	return df[['lat', 'long', 'date']]
 
-def get_unique_pairs(df): 
-	pass
+def get_unique_pairs(df, n): 
+	'''
+	Input: Pandas DataFrame, Integer
+	Output: Pandas DataFrame
+
+	For the inputted data frame, take the unique lat, long, date pairs, go back n amount of days (i.e. 
+	how many days back do we want weather), and then again get unique pairs (make sure that none of the days
+	back that we went are overlapping). 
+	'''
+
+	df['date'] = pd.to_datetime(df['date'])
+
+	pairs_set = set([(row[0], row[1], row[2]) for row in df.values])
+	pairs_set = set(get_n_back(pairs_set, n))
 
 if __name__ == '__main__': 
 	if len(sys.argv) == 1: 
-		with open('makefiles/year_list.pkl') as f: 
+		with open('../makefiles/year_list.pkl') as f: 
 			year_list = pickle.load(f)
 	elif len(sys.argv) == 2: 
 		with open(sys.argv[1]) as f: 
@@ -30,3 +44,4 @@ if __name__ == '__main__':
 
 	for year in year_list: 
 		df = get_lat_long_time(year)
+		get_unique_pairs(df, 3)
