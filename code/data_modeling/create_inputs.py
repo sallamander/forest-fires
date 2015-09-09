3588c321e64f7a95efd5aac0c755d544ca449332
sallamander <ssall@alumni.nd.edu>
2015-09-08 18:37:46 -0600

    Initlize data_modeling folder, and create_inputs.py file within it

---
+++ code/data_modeling/create_inputs.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+os.chdir('../data_manip')
+from general_featurization import grab_columns, return_all_dummies
+from time_featurization import break_time_col
+
+
+if __name__ == '__main__': 
+	pass
