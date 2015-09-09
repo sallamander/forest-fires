5ed8fd8b885825a5e185add1772a78d3240b4e6a
sallamander <ssall@alumni.nd.edu>
2015-09-08 20:01:59 -0600

    Debug grab_columns in general_featurization

--- code/modeling/create_inputs.py
+++ code/modeling/create_inputs.py
@@ -8,17 +8,16 @@ from data_manip.time_featurization import break_time_col
 
 
 if __name__ == '__main__': 
-	if len(sys.argv) == 1: 
-		with open('../makefiles/year_list.pkl') as f: 
-			year_list = pickle.load(f)
-	elif len(sys.argv) == 2: 
-		with open(sys.argv[1]) as f: 
-			year_list = pickle.load(f)
+	with open('../makefiles/year_list.pkl') as f: 
+		year_list = pickle.load(f)
+	with open('../makefiles/columns_list.pkl') as f: 
+		columns_list = pickle.load(f)
 
 	dfs_list = []
 	for year in year_list: 
 		df_path = '../../data/csvs/fires_' + str(year) + '.csv'
 		df = pd.read_csv(df_path)
 		dfs_list.append(df)
-		
+
 	df = combine_dfs(dfs_list)
+	df = grab_columns(df, columns_list)
