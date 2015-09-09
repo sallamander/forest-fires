f56ee3d6add18191e19d6ccd4fbdb0b75c29eff8
sallamander <ssall@alumni.nd.edu>
2015-09-09 10:25:35 -0600

    Ported logistic code over to run_model.py; made more modularized so it will accept other models

--- code/modeling/run_model.py
+++ code/modeling/run_model.py
@@ -1,5 +1,7 @@
 import sys
 from sklearn.linear_model import LogisticRegression
+from scoring import return_scores
+from data_manip import tt_splits
 
 def get_model(model_name): 
 	'''
@@ -11,18 +13,48 @@ def get_model(model_name):
 		lr = LogisticRegression()
 		return lr
 
-def fit_model(df, model_to_fit):
+def fit_model(train_data, model_to_fit):
 	'''
-	Input: PandasDataFrame
+	Input: Pandas DataFrame, Instantiated Model
 	Output: Fitted model
+
+	Using the fire column as the target and the remaining columns as the features, fit 
+	the inputted model. 
 	'''
 
-	target = df.fire
-	features = df.drop('fire', axis=1)
+	target = train_data.fire
+	features = train_data.drop('fire', axis=1)
 
 	model_to_fit.fit(features, target)
 	return model_to_fit
 
+def predict_with_model(test_data, model): 
+	'''
+	Input: Pandas DataFrame, Fitted Model
+	Output: Numpy Array of Predictions
+
+	Using the fitted model, make predictions with the test data and return those predictions. 
+	'''
+
+	target = test_data.fire
+	features = test_data.drop('fire', axis=1)
+
+	predictions, predicted_probs = lr.predict(features), lr.predict_proba(features)
+
+	return predictions, predicted_probs
+
 
 if __name__ == '__main__': 
+	# sys.argv[1] will hold the name of the model we want to run (logit, random forest, etc.), 
+	# and sys.argv[2] will hold our input dataframe (data will all the features and target). 
 	model_name = sys.argv[1]
+
+	with open(sys.argv[2]) as f: 
+		input_df = pickle.load(f)
+
+	train, test = tt_splits(input_df)
+	model = get_model(model_name)
+	fitted_model = fit_model(train, model)
+	preds, preds_probs = predict_with_model(test, fitted_model)
+
+
