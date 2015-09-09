4bafe813433ff583af64cc62c7f0430434a1a3d0
sallamander <ssall@alumni.nd.edu>
2015-09-09 08:22:41 -0600

    Add run_logistic function to code/modeling/logistic.py

--- code/modeling/logistic.py
+++ code/modeling/logistic.py
@@ -1,7 +1,17 @@
 from sklearn.linear_model import LogisticRegression 
 
 def run_logistic(df):
-	pass
+	'''
+	Input: PandasDataFrame
+	Output: Fitted Sklearn Logistic Model
+	'''
+
+	lr = LogisticRegression()
+	target = df.fire
+	features = df.drop('fire', axis=1)
+
+	lr.fit(features, target)
+	return lr
 
 if __name__ == '__main__': 
 	pass 
