9db118acc14928e2bf70bba0b1a81a53f5232e83
sallamander <ssall@alumni.nd.edu>
2015-09-09 09:11:27 -0600

    Finished first run at code for logistic.py; will probably re-factor and create 1 file that takes in a model and does all my fitting/predictting, etc.

--- code/modeling/logistic.py
+++ code/modeling/logistic.py
@@ -1,6 +1,9 @@
 from sklearn.linear_model import LogisticRegression 
+from data_manip import tt_splits
+from scoring import return_scores
+import pickle
 
-def run_logistic(df):
+def fit_logistic(df):
 	'''
 	Input: PandasDataFrame
 	Output: Fitted Sklearn Logistic Model
@@ -13,5 +16,25 @@ def run_logistic(df):
 	lr.fit(features, target)
 	return lr
 
+def predict_with_logistic(lr, test_data): 
+	'''
+	Input: Fitted Logistic Model, Pandas DataFrame
+	Output: Numpy Array of Predictions
+
+	Using the fitted model, make predictions with the test data and return those predictions. 
+	'''
+
+	target = df.fire
+	features = df.drop('fire', axis=1)
+
+	predictions, predicted_probs = lr.predict(features), lr.predict_proba(features)
+
+	return predictions, predicted_probs
+
 if __name__ == '__main__': 
-	pass 
+	with open('input_df.pkl') as f: 
+		input_df = pickle.load(f)
+
+	train, test = tt_splits(input_df)
+	fitted_logit = fit_logistic(train)
+	preds, pred_probs = predict_with_logistic(fitted_logit, test)
