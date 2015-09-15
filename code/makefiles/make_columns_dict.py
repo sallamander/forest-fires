3c089597483ef5e062dc5c291f7e9d39c709b850
sallamander <ssall@alumni.nd.edu>
2015-09-14 18:31:27 -0600

    Run a couple more models; time to throw this up to AWS

--- code/makefiles/make_columns_dict.py
+++ code/makefiles/make_columns_dict.py
@@ -1,6 +1,6 @@
 import pickle
 
-columns_dict = {'year': 'all_dummies', 'month': 'all_dummies', 'fire_bool': 'bool_col'}
+columns_dict = {'year': 'all_dummies', 'month': 'all_dummies', 'fire_bool': 'bool_col', 'state_name': 'all_dummies'}
 
 with open('makefiles/columns_dict.pkl', 'w+') as f: 
 	pickle.dump(columns_dict, f)
--- code/makefiles/make_columns_list.py
+++ code/makefiles/make_columns_list.py
@@ -1,6 +1,6 @@
 import pickle
 
-columns_list = ['date', 'lat', 'long', 'conf', 'fire_bool']
+columns_list = ['state_name', 'frp', 'date', 'lat', 'long', 'conf', 'fire_bool']
 
 with open('makefiles/columns_list.pkl', 'w+') as f: 
 	pickle.dump(columns_list, f)
