a318ef549cce8958812b13a82a756ec12a9717ec
sallamander <ssall@alumni.nd.edu>
2015-09-16 12:19:11 -0600

    Update links in html; get fires_app.py home page running

--- fires_app/fires_app.py
+++ fires_app/fires_app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home(): 
+	return render_template('index.html')
 
 if __name__ == '__main__': 
 	app.run()
