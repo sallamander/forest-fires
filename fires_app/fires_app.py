345d0a20c95a89cd0c906166c6e253e1adb12a03
sallamander <ssall@alumni.nd.edu>
2015-09-16 10:34:42 -0600

    Clear out fires_app folders; Going to try using a variant builder rather than bootstrap

--- fires_app/fires_app.py
+++ fires_app/fires_app.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')	
-@app.route('/home')
-def home(): 
-	return render_template('index.html')
-
 
 if __name__ == '__main__': 
 	app.run()
