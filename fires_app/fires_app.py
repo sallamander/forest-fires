87584202a7f1b0d22722c8eb08cae9a7cb1715e0
sallamander <ssall@alumni.nd.edu>
2015-09-17 10:20:10 -0600

    Played around a little more with app

--- fires_app/fires_app.py
+++ fires_app/fires_app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home(): 
-	return render_template('index.html')
+	return render_template('index2.html')
 
 if __name__ == '__main__': 
 	app.run()
