5da2619b2979358588b1994926a345d8139a20b5
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-09-17 19:34:54 +0000

    Upload more model runs from AWS

--- code/makefiles/make_columns_dict.py
+++ code/makefiles/make_columns_dict.py
@@ -3,8 +3,7 @@ import pickle
 columns_dict = {'year': {'transformation': 'all_dummies'}, 
 				'month': {'transformation': 'all_dummies'}, 
 				'fire_bool': {'transformation': 'bool_col'}, 
-				'county_name': {'transformation': 'return_top_n', 'n': 25, 'manip_type': 'eval', 
-				'eval_string': 'fire_bool - conf', 'new_col': 'fire_conf_diff'}}
+				'urban_area_bool': {'transformation': 'bool_col'}}
 
 with open('makefiles/columns_dict.pkl', 'w+') as f: 
 	pickle.dump(columns_dict, f)
--- code/makefiles/make_columns_list.py
+++ code/makefiles/make_columns_list.py
@@ -1,6 +1,6 @@
 import pickle
 
-columns_list = ['lat', 'long', 'frp', 'date_fire', 'conf', 'fire_bool', 'county_name']
+columns_list = ['lat', 'long', 'frp', 'date_fire', 'conf', 'fire_bool', 'urban_area_bool']
 
 with open('makefiles/columns_list.pkl', 'w+') as f: 
 	pickle.dump(columns_list, f)
