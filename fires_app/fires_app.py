d91dc559c5f48a9c27772b4a8a96d0d633b50f30
sallamander <ssall@alumni.nd.edu>
2015-09-17 15:04:06 -0600

    Get index/motivation page navigation down

--- fires_app/fires_app.py
+++ fires_app/fires_app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home(): 
 	return render_template('index.html')
+
+@app.route('/about/motivation')
+def about_motivation(): 
+	return render_template('motivation.html')
 
 if __name__ == '__main__': 
 	app.run()
