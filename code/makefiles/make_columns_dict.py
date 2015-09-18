cea5aac246a1612bf8573c269ce55b349aca88c9
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-09-18 20:35:26 +0000

    Add AWS run

--- code/makefiles/make_columns_dict.py
+++ code/makefiles/make_columns_dict.py
@@ -6,7 +6,9 @@ columns_dict = {'year': {'transformation': 'all_dummies'},
 				'urban_area_bool': {'transformation': 'bool_col'}, 
 				'land_water_ratio': {'transformation': 'create_new_col', 'eval_string': 'county_aland / county_water', 
 				'delete_columns': ['county_aland', 'county_water'], 'replace_nulls': True}, 
-				'tpix': {'transformation': 'outlier_boolean', 'std_multiplier': 2}}
+				'tpix': {'transformation': 'outlier_boolean', 'std_multiplier': 2},
+				'state_name': {'transformation': 'return_top_n', 'n': 5, 'manip_type': 'eval', 
+							'eval_string': 'fire_bool - conf / 100.0', 'new_col': 'fire_conf_diff'}}
 
 with open('makefiles/columns_dict.pkl', 'w+') as f: 
 	pickle.dump(columns_dict, f)
--- code/makefiles/make_columns_list.py
+++ code/makefiles/make_columns_list.py
@@ -1,7 +1,7 @@
 import pickle
 
 columns_list = ['lat', 'long', 'frp', 'date_fire', 'conf', 'fire_bool', 'urban_area_bool', 
-				'county_aland', 'county_water', 'gmt', 'temp', 'tpix']
+				'county_aland', 'county_water', 'gmt', 'temp', 'tpix', 'state_name']
 
 with open('makefiles/columns_list.pkl', 'w+') as f: 
 	pickle.dump(columns_list, f)
