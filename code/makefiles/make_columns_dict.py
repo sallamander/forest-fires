b726b312456fac1c5bad0d5407a6c0d723411209
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-09-22 01:21:07 +0000

    Add another AWS run

--- code/makefiles/make_columns_dict.py
+++ code/makefiles/make_columns_dict.py
@@ -5,7 +5,8 @@ columns_dict = {'year': {'transformation': 'all_dummies'},
 				'fire_bool': {'transformation': 'bool_col'}, 
 				'urban_area_bool': {'transformation': 'bool_col'}, 
 				'land_water_ratio': {'transformation': 'create_new_col', 'eval_string': 'county_aland / county_water', 
-				'delete_columns': ['county_aland', 'county_water'], 'replace_nulls': True}}
+				'delete_columns': ['county_aland', 'county_water'], 'replace_nulls': True}, 
+				'add_nearby_fires': {'dist_measure': 0.001, 'time_measure': 1}}
 
 with open('makefiles/columns_dict.pkl', 'w+') as f: 
 	pickle.dump(columns_dict, f)
