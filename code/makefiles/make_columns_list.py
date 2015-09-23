38dc458a05bf2193890c1520aa5ed8571af6680c
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-09-23 02:37:22 +0000

    Add a couple more AWS runs

--- code/makefiles/make_columns_list.py
+++ code/makefiles/make_columns_list.py
@@ -1,7 +1,7 @@
 import pickle
 
 columns_list = ['lat', 'long', 'frp', 'date_fire', 'conf', 'fire_bool', 'urban_area_bool', 
-				'county_aland', 'county_water', 'gmt', 'temp']
+				'county_aland', 'county_water', 'gmt', 'temp', 'dewPoint']
 
 with open('makefiles/columns_list.pkl', 'w+') as f: 
 	pickle.dump(columns_list, f)
