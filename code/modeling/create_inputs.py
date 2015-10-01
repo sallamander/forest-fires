fd8d1ef129876445517c24a9d0aa57440eaa9ff1
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-10-01 20:54:56 +0000

    Adjust create_inputs.py to not have to re-run nearby_fires creation; clean up run_model

--- code/modeling/create_inputs.py
+++ code/modeling/create_inputs.py
@@ -23,23 +23,31 @@ if __name__ == '__main__':
 
 
 	df = combine_dfs(dfs_list)
-
-	df.to_csv('master.csv')
-	df = grab_columns(df, columns_list)
 	df = break_time_col(df, 'date_fire')
 
-	if 'add_nearby_fires' in columns_dict.keys(): 
+	nearby_fires_df_filepath = './modeling/nearby_done_df_' + str(columns_dict['add_nearby_fires']['dist_measure']) + '.pkl'
+	# This process takes awhile, and I won't need to do it each time. 
+	if 'nearby_fires_done' not in columns_dict.keys(): 
 		dist_measure = columns_dict['add_nearby_fires']['dist_measure']
 		time_measures = columns_dict['add_nearby_fires']['time_measures']
 		df = gen_nearby_fires_count(df, dist_measure, time_measures)
+		with open(nearby_fires_df_filepath, 'w') as f: 
+			pickle.dump(df, f)
+	else: 
+		with open(nearby_fires_df_filepath) as f:
+			df = pickle.load(f)
 
+	import pdb
+	pdb.set_trace()
 
+	df = grab_columns(df, columns_list)
 	featurization_dict = {'all_dummies': return_all_dummies, 'bool_col': boolean_col, 'return_top_n': return_top_n, 
 						'create_new_col': create_new_col, 'outlier_boolean': return_outlier_boolean}
 
 	for k, v in columns_dict.iteritems(): 
 		if k != 'add_nearby_fires':
 			df = featurization_dict[v['transformation']](df, k, v)
-			
-	with open('./modeling/input_df.pkl', 'w+') as f: 
+	
+	input_df_filename = './modeling/input_df_' + str(columns_dict['add_nearby_fires']['dist_measure']) + '.pkl'
+	with open(input_df_filename, 'w+') as f: 
 		pickle.dump(df, f)
