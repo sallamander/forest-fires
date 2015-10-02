2c2826c3a1026320710bd224f12fb6de779c1dfe
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-10-02 12:29:21 +0000

    Run models with own grid search

--- code/modeling/run_models.sh
+++ code/modeling/run_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'Running Logistic Model'
-tmux new -d -s logit 'python modeling/run_model.py logit modeling/input_df.pkl'
+tmux new -d -s logit 'python modeling/run_model.py logit modeling/input_df_0.1.pkl'
 
 echo 'Running Random Forest Model'
-tmux new -d -s random_forest 'python modeling/run_model.py random_forest modeling/input_df.pkl'
+tmux new -d -s random_forest 'python modeling/run_model.py random_forest modeling/input_df_0.1.pkl'
 
 echo 'Running Gradient Boosting Model'
-tmux new -d -s g_boosting 'python modeling/run_model.py gradient_boosting modeling/input_df.pkl'
+tmux new -d -s g_boosting 'python modeling/run_model.py gradient_boosting modeling/input_df_0.1.pkl'
