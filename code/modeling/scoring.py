d21e6aa47948f257fa6c3971a460ca68528a1eed
Ubuntu <ubuntu@ip-172-31-59-124.ec2.internal>
2015-10-09 19:28:50 +0000

    Add couple more runs from AWS; update validation strategies

--- code/modeling/scoring.py
+++ code/modeling/scoring.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score, precision_score, roc_auc_score, recall_score
+import numpy as np
 
 def return_scores(y_true, y_pred, y_pred_probs): 
 	'''
@@ -15,6 +16,18 @@ def return_scores(y_true, y_pred, y_pred_probs):
 		if score_metric.func_name == 'roc_auc_score': 
 			score_dictionary[score_metric.func_name] = score_metric(y_true, y_pred_probs[:, 1])
 		else: 
-			score_dictionary[score_metric.func_name] = score_metric(y_true, y_pred)
+			scores = np.array([score_metric(y_true, return_y_preds(y_pred_probs, threshold)) for threshold in xrange(0, 101)])
+			score_dictionary[score_metric.func_name] = scores.max()
 
 	return score_dictionary
+
+def return_y_preds(y_pred_probs, threshold): 
+	'''
+	Input: Numpy Array, Integer
+	Output: Numpy Array
+
+	For the inputted array of predicted probabilities, return a numpy array that is 1's or 0's, 
+	where 1's are those predicted probabilities that are above the threshold.
+	'''
+
+	return y_pred_probs[:, 1] > (threshold / 100.0)
