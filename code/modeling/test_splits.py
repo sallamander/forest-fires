63110fd1f6d2cd646d0563e5e67a3ed84fefd37c
sallamander <ssall@alumni.nd.edu>
2015-10-12 10:15:50 -0600

    Update tt_splits to handle days back

--- code/modeling/test_splits.py
+++ code/modeling/test_splits.py
@@ -1,6 +1,7 @@
 import pickle
 import sys
 import datetime
+import pandas as pd
 from data_manip.tt_splits import tt_split_all_less_n_days, tt_split_early_late, tt_split_same_months
 
 
@@ -10,22 +11,24 @@ if __name__ == '__main__':
 
 	days_back = 60
 	train, test = tt_split_all_less_n_days(input_df, days_back=days_back)
+        train.loc[:, 'date_fire'] = pd.to_datetime(train['date_fire'].copy())
 
-	for months_forward in xrange(0, 132, 2): 
-                date_split = datetime.date(2013, 1, 1)
-                '''
-                training_set, validation_set = tt_split_same_months(train, 2013, [months_forward, months_forward + 1], 60)
-                '''
-                training_set, validation_set = tt_split_early_late(train, date_split, months_forward, months_backward=0.5, days_forward=2, weeks_forward=months_forward)
-                
-                '''
-                print training_set.month.unique(), training_set.year.unique()
-                print validation_set.month.unique(), validation_set.year.unique()
-                print training_set.fire_bool.mean(), validation_set.fire_bool.mean()
+	for months_forward in xrange(0, 78, 2): 
+                date_split = train.date_fire.max() - datetime.timedelta(weeks=months_forward)
+                training_set, validation_set = tt_split_same_months(train, 2013, [1], days_back=14, exact_split_date = date_split, direct_prior_days=False, add_test=True)
+
+                # training_set, validation_set = tt_split_early_late(train, date_split, months_forward, months_backward=None, days_forward=2, weeks_forward=months_forward)
+                print months_forward
+                for year in training_set.year.unique(): 
+                        print training_set.query('year == @year').date_fire.min(), training_set.query('year == @year').date_fire.max()
+                print 'on to validation'
+                for year in validation_set.year.unique(): 
+                        print validation_set.query('year == @year').date_fire.min(), validation_set.query('year == @year').date_fire.max()
                 print '\n' * 2
-                '''
 
+                '''
                 print training_set.date_fire.min(), training_set.date_fire.max()
                 print validation_set.date_fire.min(), validation_set.date_fire.max()
                 print training_set.fire_bool.sum(), validation_set.fire_bool.sum()
                 print '\n' * 2
+                '''
