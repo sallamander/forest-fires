d27ce281a99f1ebd67eb41b7c078ff0075d71b96
sallamander <ssall@alumni.nd.edu>
2015-10-16 17:05:44 -0600

    Updated makefile to handle all data downloading; delete python files for it

--- data/mk_zipped_files_folders.sh
+++ data/mk_zipped_files_folders.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
 mkdir zipped_files 
+mkdir unzipped_files 
 
-in_zipped_files=( boundary_files detected_fires )
+in_zip_unzip_files=( boundary_files detected_fires )
 in_boundary_files=( county fire_perimeters region state urban_areas )
 in_detected_fires=( MODIS )
 
-for folder_name in "${in_zipped_files[@]}"
+for folder_name in "${in_zip_unzip_files[@]}"
 do 
     mkdir zipped_files/$folder_name
+    mkdir unzipped_files/$folder_name
 done 
 
 for folder_name in "${in_boundary_files[@]}"
 do 
     mkdir zipped_files/boundary_files/$folder_name
+    mkdir unzipped_files/boundary_files/$folder_name
+    if [ "$folder_name" = "fire_perimeters" ]  
+    then
+        mkdir unzipped_files/boundary_files/$folder_name/2012
+        mkdir unzipped_files/boundary_files/$folder_name/2013
+        mkdir unzipped_files/boundary_files/$folder_name/2014
+        mkdir unzipped_files/boundary_files/$folder_name/2015 
+    else 
+        mkdir unzipped_files/boundary_files/$folder_name/2013
+        mkdir unzipped_files/boundary_files/$folder_name/2014
+    fi 
 done 
 
 for folder_name in "${in_detected_fires[@]}"
 do 
     mkdir zipped_files/detected_fires/$folder_name
+    mkdir unzipped_files/detected_fires/$folder_name
+    mkdir unzipped_files/detected_fires/$folder_name/2012
+    mkdir unzipped_files/detected_fires/$folder_name/2013
+    mkdir unzipped_files/detected_fires/$folder_name/2014
+    mkdir unzipped_files/detected_fires/$folder_name/2015 
 done 
 
 
