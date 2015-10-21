4fe37b1ea06b689d951bd971d45ef786870b43db
sallamander <ssall@alumni.nd.edu>
2015-10-20 18:58:46 -0600

    create_postgres_dts.py file cleaned up

--- code/data_setup/create_postgres_dts.py
+++ code/data_setup/create_postgres_dts.py
@@ -6,38 +6,42 @@ import pandas as pd
 
 def create_fire_db(year): 
 	'''
-	Input: Integer, String
-	Output: PSQL Table
-
-	The basefile_name read in will point to a set of shapefiles. This function will read those shapefiles
-	into a psql table. 
+	Input: Integer 
+	Output: PSQL Table 
+        
+        For the given year, generate two strings, one that holds the filepath 
+        to where the unzipped detected fires dataset for that year is being 
+        held, and the other that holds what the data table will be named in 
+        postgres. Input those two strings into the create_dt function, 
+        which will actually create the data table in our forest_fires postgres
+        data base. 
 	'''
+
 	filepath = '../data/unzipped_files/detected_fires/MODIS/' + str(year)
 
 	dt_name = 'detected_fires_' + str(year)
-	create_db(filepath, dt_name)
+	create_dt(filepath, dt_name)
 
-def create_shapefile_db(name, year): 
+def create_shapefile_db(name, year, latin_encoding=False): 
 	'''
-	Input: String, Integer
+	Input: String, Integer, Boolean
 	Output: PSQL Table
 
-	For the given string name (county, fire, or urban), read in the shapefile for that geometry and create a 
-	psql table from it. The county shapefile contains state labels as well, so that's why I'm not reading in 
-	any state shapefiles. 
+        For the given year, generate two strings, one that holds the filepath 
+        to where the unzipped shapefile dataset for that year is being 
+        held, and the other that holds what the data table will be named in 
+        postgres. Input those two strings into the create_dt function, 
+        which will actually create the data table in our forest_fires postgres
+        data base. 
 	'''
-	latin_encoding = False
 
 	if name == 'perimeters': 
 		filepath = '../data/unzipped_files/boundary_files/fire_perimeters/' + str(year)
 	elif name == 'county': 
 		filepath = '../data/unzipped_files/boundary_files/county/' + str(year)
-		latin_encoding = True
 	elif name == 'urban': 
-		latin_encoding = True
 		filepath = '../data/unzipped_files/boundary_files/urban_areas/' + str(year)
 	elif name == 'region': 
-		latin_encoding = True
 		filepath = '../data/unzipped_files/boundary_files/region/' + str(year)
 	elif name == 'state': 
 		filepath = '../data/unzipped_files/boundary_files/state/' + str(year)
@@ -45,37 +49,38 @@ def create_shapefile_db(name, year):
 		raise Exception('No boundary folder name put in... Try again!')
 
 	dt_name = name + '_shapefiles_' + str(year)
-	create_db(filepath, dt_name, latin_encoding)
+	create_dt(filepath, dt_name, latin_encoding)
 
-def create_db(filepath, dt_name, latin_encoding=False): 
+def create_dt(filepath, dt_name, latin_encoding=False): 
 	'''
-	Input: String, String
+	Input: String, String, Boolean
 	Output: PSQL Table
 
-	Create a data table named dt_name in the forest_fires database from the shapefiles located in the folder 
-	at filepath. 
+	Create a data table named dt_name in the forest_fires database from the 
+        shapefiles located in the folder at filepath. The latin_encoding boolean 
+        makes sure to adjust the way the data is encoded when being read in. 
+        For a couple of the shapefiles, it allows the data to be read in properly. 
 	''' 
-	print 'In ' + dt_name
-	if dt_exist(dt_name): 
-		if dt_name.find('2015') != -1: 
-			print 'found it'
-			dt_name = dt_name + '_temp'
-		else: 
-			return 
-	print 'out_of_it'
-	# I'm using the overwrite option here because anytime this I'm creating a db I'll be fixing something 
-	# and need to create the database from scratch. Otherwise, I won't be running this function. 
-	create_db_command = 'ogr2ogr -f "PostgreSQL" PG:"dbname=forest_fires user=' + os.environ['USER'] + \
-						'"' + ' ' + filepath \
-						+ ' -nlt PROMOTE_TO_MULTI -nln ' + dt_name + ' -overwrite'
 
-	# Some of the shapefiles (county and urban area) need to be latin-encoded to read in correctly. 
+        # Issue a warning and abort if table already exists.
+        if dt_exists(dt_name): 
+            raise Exception('This table already exists!)
+
+	# I'm using the overwrite option here because anytime this I'm creating a 
+        # db I'll be fixing something and need to create the database from scratch. 
+        # Otherwise, I won't be running this function. 
+
 	if latin_encoding:
-		create_db_command = 'PGCLIENTENCODING=LATIN1 ogr2ogr -f "PostgreSQL" PG:"dbname=forest_fires user=' + os.environ['USER'] + \
-					'"' + ' ' + filepath \
-					+ ' -nlt PROMOTE_TO_MULTI -nln ' + dt_name + ' -overwrite'
+		create_dt_command = 'PGCLIENTENCODING=LATIN1 ogr2ogr -f "PostgreSQL" ' + \
+                                    'PG:"dbname=forest_fires user=' + os.environ['USER'] + \
+				    '"' + ' ' + filepath + ' -nlt PROMOTE_TO_MULTI -nln ' \
+                                    + dt_name + ' -overwrite'
+        else: 
+	    create_dt_command = 'ogr2ogr -f "PostgreSQL" PG:"dbname=forest_fires ' + \
+                                'user=' + os.environ['USER'] + '"' + ' ' + filepath \
+			        + ' -nlt PROMOTE_TO_MULTI -nln ' + dt_name + ' -overwrite'
 
-	os.system(create_db_command)
+	os.system(create_dt_command)
 
 def dt_exist(dt_name): 
 	'''
@@ -83,7 +88,7 @@ def dt_exist(dt_name):
 	Output: Boolean 
 
 	Take in the string of the datatable we are trying to create in the forest_fires 
-	Database, test if that datatable already exists, and return a boolean for whether
+	database, test if that datatable already exists, and return a boolean for whether
 	or not it does. 
 	'''
 
@@ -91,31 +96,22 @@ def dt_exist(dt_name):
 	cursor = conn.cursor()
 
 	try: 
-		cursor.execute('SELECT * \
-						FROM ' + dt_name + ' \
-						LIMIT 1;')
+		cursor.execute('SELECT * FROM ' + dt_name + ' LIMIT 1;')
 	except: 
 		return False
 
 	return True 
 
 if __name__ == '__main__': 
-	if len(sys.argv) == 1: 
-		with open('makefiles/year_list.pkl') as f: 
-			year_list = pickle.load(f)
-	elif len(sys.argv) == 2: 
-		with open(sys.argv[1]) as f: 
-			year_list = pickle.load(f)
+    for year in xrange(2012, 2016): 
+        create_fire_db(year)
+        create_shapefile_db('perimeters', year)
 
-	for year in year_list: 
-		create_fire_db(year)
-		# create_shapefile_db('perimeters', year)
-		# The 2015 boundary shapefiles for these regions are not yet available. Later when merging we'll 
-		# just use 2014, because that will be close enough. 
-		if year != 2015 and year!= 2012: 
-			create_shapefile_db('county', year)
-			create_shapefile_db('state', year)
-			create_shapefile_db('urban', year)
-			create_shapefile_db('region', year)
-
-		
+        # We only have the following perimter shapefiles for 2013 and 2014. 
+        if year not in (2012, 2015): 
+            create_shapefile_db('county', year, latin_encoding=True)
+            create_shapefile_db('state', year)
+            create_shapefile_db('urban', year, latin_encoding=True)
+            create_shapefile_db('region', year, latin_encoding=True)
+            
+        
