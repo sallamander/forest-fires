1c06c904ae07b16523dff2331ce7de834ef7e371
sallamander <ssall@alumni.nd.edu>
2015-10-21 11:55:18 -0600

    Update for loops to use arrays

--- data/mk_folders.sh
+++ data/mk_folders.sh
@@ -6,6 +6,9 @@ mkdir unzipped_files
 in_zip_unzip_files=( boundary_files detected_fires )
 in_boundary_files=( county fire_perimeters region state urban_areas )
 in_detected_fires=( MODIS )
+fire_tbl_years=( 2012 2013 2014 2015 ) 
+boundary_tbl_years=( 2013 2014 )
+
 
 for folder_name in "${in_zip_unzip_files[@]}"
 do 
@@ -19,13 +22,15 @@ do
     mkdir unzipped_files/boundary_files/$folder_name
     if [ "$folder_name" = "fire_perimeters" ]  
     then
-        mkdir unzipped_files/boundary_files/$folder_name/2012
-        mkdir unzipped_files/boundary_files/$folder_name/2013
-        mkdir unzipped_files/boundary_files/$folder_name/2014
-        mkdir unzipped_files/boundary_files/$folder_name/2015 
+        for year in "${fire_tbl_years[@]}"
+        do 
+            mkdir unzipped_files/boundary_files/$folder_name/$year
+        done 
     else 
-        mkdir unzipped_files/boundary_files/$folder_name/2013
-        mkdir unzipped_files/boundary_files/$folder_name/2014
+        for year in "${boundary_tbl_years[@]}"
+        do 
+            mkdir unzipped_files/boundary_files/$folder_name/$year
+        done 
     fi 
 done 
 
@@ -33,10 +38,11 @@ for folder_name in "${in_detected_fires[@]}"
 do 
     mkdir zipped_files/detected_fires/$folder_name
     mkdir unzipped_files/detected_fires/$folder_name
-    mkdir unzipped_files/detected_fires/$folder_name/2012
-    mkdir unzipped_files/detected_fires/$folder_name/2013
-    mkdir unzipped_files/detected_fires/$folder_name/2014
-    mkdir unzipped_files/detected_fires/$folder_name/2015 
+
+    for year in "${fire_tbl_years[@]}"
+    do 
+        mkdir unzipped_files/detected_fires/$folder_name/$year
+    done 
 done 
 
 
