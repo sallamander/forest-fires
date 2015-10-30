31b3a485d90ccf6bca89d0d9284cca419a617a14
sallamander <ssall@alumni.nd.edu>
2015-10-30 10:05:54 -0600

    Updated geo_featurization.py to more efficiently query df for nearby fires; updated other relevant files used in that process

--- code/modeling/data_manip/geo_featurization.py
+++ code/modeling/data_manip/geo_featurization.py
@@ -3,126 +3,115 @@ import numpy as np
 import multiprocessing
 from itertools import izip
 from datetime import timedelta
+from functools import partial 
 
-def gen_nearby_fires_count(df, dist_measure, time_measures, rate_measures):
-	'''
-	Input: Pandas DataFrame, Float, List
-	Output:  Pandas DataFrame
+def gen_nearby_fires_count(df, dist_measure, time_measures):
+    '''
+    Input: Pandas DataFrame, Float, List
+    Output:  Pandas DataFrame
 
-	For each row in the detected fires data set, create a new column that is the count of nearby 
-	potential detected fires, where nearby detected fires are determined by the inputted dist_measure
-	and time_measures list.  
-	'''
-	for time_measure in time_measures: 
-		query_prep_clean(df, dist_measure, time_measure, beginning = True) 
-		pool = multiprocessing.Pool(multiprocessing.cpu_count())
-		nearby_count_dict = pool.map(query_for_nearby_fires, df2.values) 
-		nearby_count_df = pd.DataFrame(nearby_count_dict)
-		df = merge_results(df, nearby_count_df)
-		query_prep_clean(df, dist_measure, time_measure, beginning=False)
+    For each row in the detected fires data set, create a new column that is 
+    the count of nearby potential detected fires, as well as a count of the 
+    nearby actually detected fires (here we will be look only 1+ days back 
+    since we won't have that information for the current day in real time), 
+    where nearby detected fires are determined by the inputted dist_measure 
+    and time_measures list.  
+    '''
+    lat_idx, long_idx, date_idx = grab_col_indices(df, ['lat', 'long', 'date_fire'])
+    df = df.drop_duplicates(['lat', 'long', 'date_fire'])
+    for time_measure in time_measures: 
+        pool = multiprocessing.Pool(3)
+        execute_query = partial(query_for_nearby_fires, df, 
+                                dist_measure, time_measure, lat_idx, 
+                                long_idx, date_idx)
+        nearby_count_dict = pool.map(execute_query, df.values[0:100]) 
+        df = merge_results(df, nearby_count_dict)
 
-	if rate_measures == True: 
-		for time_measure in time_measures[:-1]: 
-			df = gen_rate_nearby_fires(df, time_measure)
+    return df
 
-	return df
+def query_for_nearby_fires(df, dist_measure, time_measure, 
+                        lat_idx, long_idx, date_idx, row): 
+    '''
+    Input: Numpy Array, Pandas DataFrame, Numpy Array, Float, Integer, 
+            Integer, Integer, Integer
+    Output: Dictionary 
 
-def query_for_nearby_fires(row): 
-	'''
-	Input: Pandas DataFrame, Numpy Array, Float, Integer
-	Output: Float
+    For the inputted row, query the dataframe for the number of 
+    'detected fires' (i.e. other rows) that are within dist_measure 
+    of the inputted row (lat/long wise)  and within the time_measure 
+    of the inputted row (date wise). 
+    '''
+    lat, lng, date = row[lat_idx], row[long_idx], row[date_idx]
+    lat_min, lat_max = lat - dist_measure, lat + dist_measure
+    long_min, long_max = lng - dist_measure, lng + dist_measure
 
-	For the inputted row, query the dataframe for the number of 'detected fires' (i.e. other rows) that 
-	are within dist_measure of the inputted row and within the time_measure of the inputted row. 
-	'''
-	lat, lng, date = row[lat_idx], row[lng_idx], row[dt_idx]
-	lat_min, lat_max = lat - dist_measure2, lat + dist_measure2
-	long_min, long_max = lng - dist_measure2, lng + dist_measure2
-	# Note we're not doing day_max here because in real time we wouldn't have forward dates. 
-	date_min  = date - timedelta(days=time_measure2)
-	query = '''lat >= @lat_min and lat <= @lat_max and long >= @long_min and long <= @long_max and date_temp >= @date_min and date_temp <= @date'''
-	nearby_count = df2.query(query).shape[0]
+    # Note we're not doing day_max here because in real time we wouldn't have forward dates. 
+    if time_measure == 0: 
+        hour, minute, second = date.hour, date.minute, date.second
+        date_min = date - timedelta(hours=hour)
+        date_min = date_min - timedelta(minutes=minute)
+        date_min = date_min - timedelta(seconds = second)
+    else: 
+        date_min  = date - timedelta(days=time_measure)
 
-	nearby_count_label = 'nearby_count_' + str(time_measure2)	
-	output_dict = {'lat': lat, 'long': lng, 'date_count': date, nearby_count_label: nearby_count}
-	return output_dict
+    all_nearby_query = '''lat >= @lat_min and lat <= @lat_max and long >= @long_min and long <= @long_max and date_fire >= @date_min and date_fire <= @date'''
+    nearby_fires_query = all_nearby_query + " and fire_bool == True" 
+    all_nearby_count = df.query(all_nearby_query).shape[0]
+    nearby_fires_count = df.query(nearby_fires_query).shape[0]
 
-def query_prep_clean(df, dist_measure, time_measure, beginning=True): 
-	'''
-	Input: Pandas DataFrame, Float, Float, Boolean
-	Output: Global Variables (deleted or created)
+    all_nearby_count_label = 'all_nearby_count' + str(time_measure)	
+    nearby_fires_count_label = 'all_nearby_fires' + str(time_measure)	
+    output_dict = {'lat': lat, 'long': lng, 'date_fire': date, 
+                    all_nearby_count_label: all_nearby_count, 
+                    nearby_fires_count_label: nearby_fires_count}
 
-	For the inputted dataframe and time dist measure, create the necessary global variables for 
-	the multiprocessing environment. Since I am multiprocessing a dataframe query, I need to be able to 
-	either pass the dataframe through the multiprocessing pool, or make it a global. While neither
-	is ideal, the second seems better. I'll make sure to delete the global when I'm all done. 
-	'''
-	if beginning == True: 
-		global df2, lat_idx, lng_idx, dt_idx, dist_measure2, time_measure2
-		dist_measure2, time_measure2 = dist_measure, time_measure
-		df2 = df.copy()
-		df2['date_temp'] = pd.to_datetime(df2['date_fire'])
-		df2 = add_hour_second(df2, 'date_temp')
-		df_columns = df2.columns
-		lat_idx, lng_idx, dt_idx = np.where(df_columns == 'lat')[0][0], np.where(df_columns == 'long')[0][0], np.where(df_columns == 'date_temp')[0][0] 
-	if beginning == False:
-		del df2, lat_idx, lng_idx, dt_idx, dist_measure2, time_measure2	
+    return output_dict
 
-def merge_results(df, nearby_fires_df): 
-	'''
-	Input: Pandas DataFrame, Pandas DataFrame
-	Output Pandas DataFrame
+def merge_results(df, nearby_count_dict): 
+    '''
+    Input: Pandas DataFrame, Dictionary
+    Output: Pandas DataFrame
 
-	Take in the nearby_fires_count_df, and merge it into the current df we are working with 
-	to run it through the model. We will be merging by lat, long, and date
-	'''
-	df['date_count'] = pd.to_datetime(df['date_fire'])
-	df = add_hour_second(df, 'date_count')
-	nearby_fires_df = nearby_fires_df.drop_duplicates()
-	df = pd.merge(df, nearby_fires_df, how='left', on=['lat', 'long', 'date_count'])
-	df.drop('date_count', axis=1, inplace=True)
+    Take in the nearby_count_dict, and merge it into the current df 
+    we are working with to run it through the model. We will be merging 
+    by lat, long, and date. 
+    '''
 
-	return df
+    nearby_fires_df = pd.DataFrame(nearby_count_dict)
+    nearby_fires_df = nearby_fires_df.drop_duplicates()
+    df = pd.merge(df, nearby_fires_df, how='inner', on=['lat', 'long', 'date_fire'])
 
-def add_hour_second(df, date_col_to_add_to): 
-	'''
-	Input: Pandas DataFrame 
-	Output: Pandas DataFrame
+    return df
 
-	Take in the data frame, and add to the date_temp the hours and seconds so that we can make sure to only grab 
-	those fires that are less than in time (where less than includes hours and seconds and not just the day). 
-	'''
-	
-	hour = []
-	minute = []
-	for gmt in df.gmt.values:
-		gmt = str(gmt)
-		if len(gmt) == 3: 
-			hour.append(int(gmt[0]))
-			minute.append(int(gmt[1:]))
-		else: 
-			hour.append(int(gmt[0:2]))
-			minute.append(int(gmt[2:]))
+def grab_col_indices(df, col_list):
+    '''
+    Input: Pandas DataFrame
+    Output: Tuple
 
-	df['hour'] = hour
-	df['minute'] = minute
-	df[date_col_to_add_to] = [date_temp + pd.Timedelta(hours=hour) + pd.Timedelta(minutes=minute) for date_temp, hour, minute in \
-			  izip(df[date_col_to_add_to], df.hour, df.minute)]
+    For the inputted dataframe and list of column names, output a tuple of the 
+    column indices for those columns. 
+    '''
 
-	return df 
+    df_columns = df.columns
+    idx_list = [np.where(df_columns == col)[0][0] for col in col_list]
+
+    return tuple(idx_list)
 
 def gen_rate_nearby_fires(df, time_measure): 
-	'''
-	Input: Pandas DataFrame, Integer 
-	Output DataFrame
+    '''
+    Input: Pandas DataFrame, Integer 
+    Output DataFrame
 
-	For the inputted time_measure, find the percentage change in the number of fires from the day before to that day. 
-	I.e. if the time_measure is 6, then find the percentage change in the number of fires from 7 days prior
-	to 6 days prior. Since these columns are already created in the df, this will be pretty easy. 
-	'''
-	today_column = 'nearby_count_' + str(time_measure)
-	yesterday_column = 'nearby_count_' + str(time_measure + 1)
-	new_col_name = 'perc_change_' + str(time_measure) + '_' + str(time_measure + 1)
-	df[new_col_name] = (df[today_column] - df[yesterday_column]) / df[yesterday_column]
+    For the inputted time_measure, find the percentage change in the 
+    number of fires from the day before to that day. I.e. if the 
+    time_measure is 6, then find the percentage change in the 
+    number of fires from 7 days prior to 6 days prior. Since these 
+    columns are already created in the df, this will be pretty easy. 
+    '''
 
-	return df
+    today_column = 'nearby_count_' + str(time_measure)
+    yesterday_column = 'nearby_count_' + str(time_measure + 1)
+    new_col_name = 'perc_change_' + str(time_measure) + '_' + str(time_measure + 1)
+    df[new_col_name] = (df[today_column] - df[yesterday_column]) / df[yesterday_column]
+
+    return df
