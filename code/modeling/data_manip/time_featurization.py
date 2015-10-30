a6699e4a4063245881b1f53b36b0f184e981d2f0
sallamander <ssall@alumni.nd.edu>
2015-10-30 10:04:22 -0600

    Updated time_featurization to create date column and removed hour/day column

--- code/modeling/data_manip/time_featurization.py
+++ code/modeling/data_manip/time_featurization.py
@@ -1,15 +1,15 @@
-def break_time_col(df, time_column): 
-	'''
-	Input: Pandas DataFrame, String
-	Output: Pandas DataFrame
+from itertools import izip
+from datetime import datetime
 
-	The date/time column in our dataframe is in list format, where we have [YYYY, MM, DD]. 
-	Here we're going to break that apart and create new variables in our dataframe. We'll delete
-	the original data/time column after we're done. 
-	'''
+def add_date_column(df):
+    '''
+    Input: Pandas DataFrame
+    Output Pandas DataFrame
+    '''
+        
+    df['date_fire']=[datetime.strptime(date + "-" + str(gmt), "%Y-%m-%d-%H%M") for \
+                    date, gmt in izip(df['date'].values, df['gmt'].values)]
 
-	time_col = df[time_column]
-	df['year'] = [time[0:4] for time in time_col]
-	df['month'] = [time[5:7] for time in time_col]
+    return df
 
-	return df
+
