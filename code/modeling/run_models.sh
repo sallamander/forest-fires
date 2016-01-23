da1eae7ff46bef6fe190a018cb12315e2cbaea92
sallamander <ssall@alumni.nd.edu>
2016-01-23 12:47:49 -0700

    Update Makefile and paths for running

--- code/modeling/run_models.sh
+++ code/modeling/run_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'Running Logistic Model'
-tmux new -d -s logit 'python code/modeling/run_model.py logit code/modeling/geo_time_done.csv'
+tmux new -d -s logit 'python code/modeling/run_model.py logit code/modeling/model_input/geo_time_done.csv'
 
 echo 'Running Random Forest Model'
-tmux new -d -s random_forest 'python code/modeling/run_model.py random_forest code/modeling/geo_time_done.csv'
+tmux new -d -s random_forest 'python code/modeling/run_model.py random_forest code/modeling/model_input/geo_time_done.csv'
 
 echo 'Running Gradient Boosting Model'
-tmux new -d -s g_boosting 'python code/modeling/run_model.py gradient_boosting code/modeling/geo_time_done.csv'
+tmux new -d -s g_boosting 'python code/modeling/run_model.py gradient_boosting code/modeling/model_input/geo_time_done.csv'
