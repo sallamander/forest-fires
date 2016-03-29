7a31cefc9c4db0b655cec279a4384fbbfbdd88c1
sallamander <ssall@alumni.nd.edu>
2016-03-29 15:22:40 -0600

    Add functionality for plotting on a single day
    One of the only pieces of functionality that I hadn't built up to this
    point was the functionality to plot a county, and it's fire perimeters
    and fires for a single day. It's now here.
    
    I've also ported over the use of the StateMap and CountyMap classes to a
    USMapBuilder class. This accompanies a change in the API for the dsfuncs
    module used during EDA.

--- code/eda-viz/in_proj/grab_perim_boundary.sh
+++ code/eda-viz/in_proj/grab_perim_boundary.sh
@@ -13,16 +13,18 @@ fi
 
 year=${dt:0:4}
 
-if [ ${#dt} -gt 5 ]; then 
-    ogr2ogr -f "ESRI Shapefile" ${st_name}_$dt.shp PG:"host=localhost user=sallamander dbname=forest_fires" -sql "SELECT wkb_geometry FROM fp_sts_$year WHERE statefp = '${st_fips}' and date_ = '$dt'"
-    mv ${st_name}_$dt.* data/${st_name}/
-    ogr2ogr -f "ESRI Shapefile" -dim 2 data/${st_name}/${st_name}_${dt}_2D.shp data/${st_name}/${st_name}_$dt.shp 
-    rm data/${st_name}/${st_name}_$dt.shp
-else 
-    ogr2ogr -f "ESRI Shapefile" ${st_name}_$dt.shp PG:"host=localhost user=sallamander dbname=forest_fires" -sql "SELECT wkb_geometry FROM fp_sts_$year WHERE statefp = '${st_fips}'"
-    mv ${st_name}_$dt.* data/${st_name}/
-    ogr2ogr -f "ESRI Shapefile" -dim 2 data/${st_name}/${st_name}_${dt}_2D.shp data/${st_name}/${st_name}_$dt.shp 
-    rm data/${st_name}/${st_name}_$dt.shp
+if [ ! -f data/${st_name}/${st_name}_${dt}_2D.shp ]; then 
+    if [ ${#dt} -gt 5 ]; then 
+        ogr2ogr -f "ESRI Shapefile" ${st_name}_${dt}.shp PG:"host=localhost user=sallamander dbname=forest_fires" -sql "SELECT wkb_geometry FROM fp_sts_${year} WHERE statefp = '${st_fips}' and date_ = '${dt}';"
+        mv ${st_name}_${dt}.* data/${st_name}/
+        ogr2ogr -f "ESRI Shapefile" -dim 2 data/${st_name}/${st_name}_${dt}_2D.shp data/${st_name}/${st_name}_${dt}.shp 
+        rm data/${st_name}/${st_name}_${dt}.*
+    else 
+        ogr2ogr -f "ESRI Shapefile" ${st_name}_${dt}.shp PG:"host=localhost user=sallamander dbname=forest_fires" -sql "SELECT wkb_geometry FROM fp_sts_${year} WHERE statefp = '${st_fips}';"
+        mv ${st_name}_${dt}.* data/${st_name}/
+        ogr2ogr -f "ESRI Shapefile" -dim 2 data/${st_name}/${st_name}_${dt}_2D.shp data/${st_name}/${st_name}_${dt}.shp 
+        rm data/${st_name}/${st_name}_${dt}.* 
+    fi 
 fi 
 
 
