588c9263284e9b56c5489b93c774ab6ded081daa
sallamander <ssall@alumni.nd.edu>
2016-04-12 13:52:04 -0600

    Add county boundary interactivity to homepage.

--- code/app/static/scripts/homepage.js
+++ code/app/static/scripts/homepage.js
@@ -17,7 +17,7 @@ var path = d3.geo.path().projection(projection);
 var g = svg.append("g");
 
 d3.json("/static/scripts/data/usa_topo.json", function(error, usa) {
-    
+
     g.selectAll("path")
         .data(topojson.feature(usa, usa.objects.usa).features)
         .enter()
@@ -55,13 +55,33 @@ function clicked(d) {
     centered = null;
   }
 
-  console.log(d.properties.name); 
+  var name = d.properties.name; 
+  var name_arr = name.toLowerCase().split(' '); 
+  var state_name = name_arr.join('_')
+  var filename = state_name + '_topo.json'; 
   g.selectAll("path")
       .classed("active", centered && function(d) { return d === centered; });
+
+  g.selectAll(".county-borders").remove(); 
 
   g.transition()
       .duration(750)
       .attr("transform", "translate(" + svg_width / 2 + "," + svg_height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
       .style("stroke-width", 1.5 / k + "px");
+
+  d3.json('/static/scripts/data/county_data/county_files/' + filename, 
+          function(error, county) {
+    
+    console.log(county.objects); 
+    console.log(topojson.feature(county, county.objects[state_name])); 
+    g.selectAll(".county-borders")
+        .data(topojson.feature(county, county.objects[state_name]).features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr('class', 'county-borders')
+        .style("fill", "#D2D2D2")
+        .style("stroke", "#ffffff"); 
+  }); 
 }
 
